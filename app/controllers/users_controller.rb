ef3a85328e4b7403e01d40be878fe7e8b79973dd
colleenmcguckin <colleenmcguckin@gmail.com>
2016-09-05 17:51:14 -0700

    basic user crud

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :load_user, except: [:index]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to @user, notice: 'User has been updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy!
+      redirect_to users_path, notice: 'User has been deleted.'
+    else
+      render :show, notice: "User couldn't be deleted."
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :role
+    )
+  end
+end
