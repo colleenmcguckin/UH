3f2004f44d1baecc4f96d29e6af8de70f40d68c7
colleenmcguckin <colleenmcguckin@gmail.com>
2016-10-26 19:50:04 -0700

    start styling

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -6,4 +6,23 @@ class DonorsController < ApplicationController
     @user = Donor.find(params[:id])
   end
 
+  def edit
+    load_user
+  end
+
+  private
+
+  def load_user
+    if current_donor
+      current_user = current_donor
+      @user = Donor.find current_user
+    elsif current_receiver
+      current_user = current_receiver
+      @user = Receiver.find current_user
+    elsif current_admin
+      current_user = current_admin
+      @user = Admin.find current_user
+    end
+  end
+
 end
