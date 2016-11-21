e65b76f2ef18fda36ec1b5957d6a7ef36d3caa8d
colleenmcguckin <colleenmcguckin@gmail.com>
2016-11-20 20:07:27 -0800

    for got the migration

---
+++ db/migrate/20161121030055_add_details_to_donor.rb
@@ -0,0 +1,12 @@
+class AddDetailsToDonor < ActiveRecord::Migration
+  def change
+    add_column :donors, :agency_name, :string
+    add_column :donors, :street_address, :string
+    add_column :donors, :city, :string
+    add_column :donors, :state, :string
+    add_column :donors, :zip, :string
+    add_column :donors, :contact_name, :string
+    add_column :donors, :contact_email, :string
+    add_column :donors, :contact_phone, :string
+  end
+end
