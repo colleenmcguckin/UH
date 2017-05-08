9496cd2fa8c3f0254f3eef001c5aa5582962973e
colleenmcguckin <colleenmcguckin@gmail.com>
2017-05-08 00:51:26 -0700

    add a confirmation mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,17 +6,23 @@ class ApplicationMailer < ActionMailer::Base
     @receiver = donation.receiver
     @donor = donation.donor
     @donation = donation
-    @url =
 
     mail(to: @receiver.contact_details.first.dfr_contact_email, subject: 'Urban Harvester - You have been added to a donation!')
+  end
+
+  def confirm_donation_sent donation
+    @receiver = donation.receiver
+    @donor = donation.donor
+    @donation = donation
+
+    mail(to: @donor.contact_email, subject: 'Urban Harvester - Donation Confirmed!')
   end
 
   def donation_received_mailer donation
     @receiver = donation.receiver
     @donor = donation.donor
     @donation = donation
-    @url =
 
-    mail(to: @receiver.contact_details.first.dfr_contact_email, subject: 'Urban Harvester - Your donation has been received!')
+    mail(to: @donor.contact_email, subject: 'Urban Harvester - Your donation has been received!')
   end
 end
